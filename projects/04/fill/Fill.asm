253df672f4d440e8b3ed35533f57ca0872ef7559
gmap <gabehilism@gmail>
2023-08-21 04:41:25 -0300

    ram512, ram4k, ram16k, mult.asm and fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,78 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// R0 = value to set (0/1)
+
+@R0
+M=0
+
+@SCREEN
+M=1
+
+(LOOP)
+    @KBD // A=KBD, M=RAM[KBD]
+    D=M
+
+    @R0
+    M=-1
+    @SETSCREEN
+    D;JNE
+
+    @R0
+    M=0
+    @SETSCREEN
+    D;JMP
+
+(SETSCREEN)
+    @i // A = i, M=RAM[i]
+    M=0
+
+    //
+    // set upper to number of bits in screen (8k) - 1 
+    //
+    @16 // D = 8191
+    D=A
+    @upper // A = upper  M=RAM[upper]
+    M=D // RAM[upper] = 8191
+
+    (SSCRNLOOP)
+        // quit loop
+        @upper // A = upper, M=RAM[upper]
+        D=M
+        @i // A = i, M=RAM[i]
+        D=D-M
+        @LOOP
+        D;JEQ
+
+        //
+        // set pixel at i to value at R0
+        //
+
+        // calc pixel adress (screen+i)
+        @SCREEN // A = SCREEN, M=RAM[SCREEN]
+        D=A
+
+        @i
+        D=D+M // SCREEN + i
+
+        @scrncrd // save screen coord (ram address) to RAM[scrncrd]
+        M=D
+
+        // get target value for screen bits
+        @R0 
+        D=M 
+
+        @scrncrd // A=scrncrd, M=RAM[scrncrd]
+        A=M // get ram address to write to and point A to it
+        M=D // RAM[scrncrd] = R0
+
+        // increment i
+        @i
+        M=M+1
+
+        @SSCRNLOOP
+        0;JMP
+
+
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,51 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+// initialize counter
+
+
+@i // A = i, M = RAM[i]
+M=0 // RAM[i] = 0
+
+// initialize R2 to 0
+@R2 // A = 2, M = RAM[2]
+M=0 // RAM[2] = 0
+
+(LOOP)
+    //
+    // loop end condition, if (i == r1, or i-r1 == 0), jmp to END
+    //
+
+    // send R1 to memory register M (and A) 
+    @R1 // A = 1, M = RAM[1]
+
+    // send R1 value to M, then to data register 
+    D=M // D = RAM[1]
+
+    // subtract
+    @i // A = i, M = RAM[i]
+    D=D-M // copy of value at RAM[1] = RAM[1] - RAM[i]
+    @END
+    D;JEQ
+
+    // add R0 to R2
+    @R0 // A = 0, M = RAM[0]
+    D=M // D = RAM[0]
+
+    @R2 // A = 2, M = RAM[2]
+    M=M+D // M (RAM[2]) = D (RAM[0]) + RAM[2]
+
+    //
+    // increment i 
+    //
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
